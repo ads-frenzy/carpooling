070c354985efc1b7117331d44a7ac65edca4247d
ads-frenzy <yukthasrirangam1703@gmail.com>
2022-07-15 16:32:58 +0530

    Added isMatched and MatchedRides fields in RideRequest Class

--- lib/models/ride_request.dart
+++ lib/models/ride_request.dart
@@ -1,5 +1,14 @@
 import 'package:carpooling/models/coordinates.dart';
 import 'package:carpooling/models/ride_type.dart';
+class MatchedRides{
+  String userId;
+  String requestId;
+  MatchedRides(
+  {
+    required this.userId,
+    required this.requestId
+  });
+}
 
 class RideRequest {
 	String userId;
@@ -9,14 +18,17 @@ class RideRequest {
   Coordinates? destinationCoords;
   DateTime? dateTime;
   RideType? rideType;
-
+  bool? isMatched;
+  List<MatchedRides>? matchedRides;
   RideRequest({
     required this.userId,
     this.source,
     this.sourceCoords,
     this.destination,
     this.destinationCoords,
+    this.isMatched,
     this.dateTime,
     this.rideType,
+    this.matchedRides,
   });
 }
